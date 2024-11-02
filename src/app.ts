3e1480d877dea7f6fb79fdee449b32f0f3a3b561
Bryan Mancilla <bmancilla119@gmail.com>
2024-11-02 11:59:47 -0500

    feat: setup logger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import "dotenv/config";
 import dbConnect from "./config/dbConnect";
+import logger from "./utils/logger";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`);
+  /*  console.log(`Server is running on port ${PORT}`); */
+  logger.info(`Server is running on port ${PORT}`);
 
   await dbConnect();
 });
--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import logger from "../utils/logger";
 
 const dbConnect = async () => {
   const DB_URL = process.env.DB_URL as string;
   try {
     await mongoose.connect(DB_URL);
-    console.log("Connected to the database");
+    // console.log("Connected to the database");
+    logger.info("Connected to the database");
   } catch (error) {
-    console.error("Failed to connect to the database", error);
+    // console.error("Failed to connect to the database", error);
+    logger.error("Failed to connect to the database", error);
     process.exit(1);
   }
 };
